4415bc0122270fd286e599098cc41bc83b513f7d
Steve Stonebraker <github@brakertech.com>
2013-06-24 15:09:13 -0500

    updated ipset

--- ipset/cidr_to_ipset.sh
+++ ipset/cidr_to_ipset.sh
@@ -10,12 +10,16 @@
 # http://www.ip2location.com/free/visitor-blocker
 #
 
-
+#check if argument provided
 if [ "$#" == "0" ]; then
-    echo "No arguments provided, please provide file name"
+    echo -e "No arguments provided, please provide file name!/nExample: ./cidr_to_ipset ES.txt"
     exit 1
 fi
-#assign user's provided input to "fullfile"
+
+#Make sure ipset is installed!
+command -v ipset >/dev/null && continue || { echo "ipset command not found!  Exiting..."; exit 1; }
+
+#assign user's provided input to variable "fullfile"
 fullfile=$1
 
 #Check to make sure the user provided a valid file
@@ -44,5 +48,5 @@ do
 ipset -A ${country} $IP -exist
 done
 
-echo "ipset created and also exported to {country}.ipset"
+echo "ipset created and also exported to ${country}.ipset"
 ipset save > ${country}.ipset

551a37e2bf76d552109d6206bde0f7ff3dd8971b
Steve Stonebraker <github@brakertech.com>
2013-06-27 18:26:08 -0500

    hardcoded script to restart beaver if memory usage is above 512MB

---
+++ hardcoded_stuff/check_beaver_size.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -ex
+
+restart_service_mem_ge_x () {
+        #
+        # Description:Restart service if memory usage is greater than x
+        # usage: restart_service_mem_ge_x process_name max_size
+        #
+        # Example: restart_service_mem_ge_x httpd 1024  (restart httpd if using more than 1024MB of memory
+        process_name=$1
+        max_size=$2
+        start_service_command="$3"
+
+        actual_size=$( /bin/ps aux | /usr/bin/awk "/$process_name/"'{total+=$6}END{print total/1024}' |  /usr/bin/awk '{printf "%.0f\n", $1}' )
+
+        if [ $actual_size -ge $max_size ] ; then
+                /bin/echo "${process_name} is currently using ${actual_size}MB, which is greater than or equal to ${max_size}MB"
+                /usr/bin/pkill ${process_name}
+                $start_service_command
+        else
+                /bin/echo "${process_name} is currently using ${actual_size}MB, which is less than ${max_size}MB maximum value"
+        fi
+
+}
+
+/usr/bin/touch /var/run/logstash_beaver.pid
+/bin/chown beaver:beaver /var/run/logstash_beaver.pid
+restart_service_mem_ge_x beaver 512 "service beaver start"

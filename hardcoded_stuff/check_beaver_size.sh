0456e0b562a703b3f0a4c5a81ad57c328ef74aec
Steve Stonebraker <github@brakertech.com>
2013-06-27 18:35:06 -0500

    added if

--- hardcoded_stuff/check_beaver_size.sh
+++ hardcoded_stuff/check_beaver_size.sh
@@ -24,4 +24,15 @@ restart_service_mem_ge_x () {
 
 /usr/bin/touch /var/run/logstash_beaver.pid
 /bin/chown beaver:beaver /var/run/logstash_beaver.pid
-restart_service_mem_ge_x beaver 512 "service beaver start"
+
+if /bin/ps aux | /bin/grep "[b]eaver" > /dev/null
+then
+    echo "beaver service is running"
+    restart_service_mem_ge_x beaver 512 "service beaver start"
+else
+    echo "Starting beaver process"
+
+    service beaver start
+fi
+
+

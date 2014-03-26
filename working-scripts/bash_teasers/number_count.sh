653f1d4853a9a69be6aa7743696613209fc71ec4
Steve Stonebraker <steve.stonebraker@gmail.com>
2014-03-26 00:18:10 -0400

    initial commit

---
+++ working-scripts/bash_teasers/number_count.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# number_count.sh
+# Author: Steve Stonebraker
+# Description: Between 8000 and 9999 list the numbers that contain integers 0-9 more than once
+#
+
+
+for x in {8000..9999} ; do
+	ocount $x
+
+	egrep "0{2,4}|1{2,4}|2{2,4}|3{2,4}|4{2,4}|5{2,4}|6{2,4}|7{2,4}|8{2,4}|9{2,4}" <<< "$x" &> /dev/null
+	[[ "$?" -eq 0 ]] && echo "number $1 has integer 0-9 occuring more than once"
+
+done
+

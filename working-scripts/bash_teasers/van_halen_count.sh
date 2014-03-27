90332bf3b946b67f603aefdb6832a7a2fd3cda00
Steve Stonebraker <github@brakertech.com>
2014-03-27 11:46:11 -0400

    Added Comments, updated limit

--- working-scripts/bash_teasers/van_halen_count.sh
+++ working-scripts/bash_teasers/van_halen_count.sh
@@ -9,23 +9,61 @@
 #
 #
 
+######################################
+# Variable Setup
 # Limit concurrent curl instances
-LIMIT=40
+LIMIT=10
 
-employees=/tmp/employees
+# Tmp file for employee urls and grep results
+EMPLOYEES=/tmp/employees
+BAND_MEMBERS=/tmp/output
+######################################
 
+#############
+# Functions #
+#############
+
+###############################################
+# Count the occurances of band member in file #
+###############################################
 count_string_in_file () {
-	result=$(strings /tmp/output | grep -i "$1" | wc -l)
-	echo "${1}: ${result}" 
+  result=$(strings ${BAND_MEMBERS} | \
+    grep -i "$1" | wc -l)
+
+  echo "${1}: ${result}" 
 }
 
-# Parse Employee URLs
-curl --silent http://www.jellyvision.com/team/ | egrep -o "http:\/\/www\.jellyvision\.com\/team\/([^\/]+)\/" > ${employees}
+#######################
+# Parse Employee URLs #
+#######################
+parse_employee_urls () {
+  echo "Parsing employee urls..."
+  curl --silent http://www.jellyvision.com/team/ | \
+  egrep -o \
+  "http:\/\/www\.jellyvision\.com\/team\/([^\/]+)\/" \
+  > ${EMPLOYEES}
+}
 
-# Cut all occurances of david or sammy from each employees bio page
-tail -n +5 ${employees} | xargs -n 1 -P $LIMIT -I{} bash -c "curl --silent {} | egrep -o -i 'david lee roth|sammy hagar' " > /tmp/output
+############################################################
+# Cut all occurances of David Lee Roth or Sammy Hagar from #
+# each employee bio page and place in ${BAND_MEMBERS} file #
+#                                                          #
+# Concurrent curl connections are limited by ${LIMIT}      #
+############################################################
+get_band_members () {
+  echo "Inspecting each bio page for correct answer..."
+  tail -n +5 ${EMPLOYEES} | \
+  xargs -n 1 -P $LIMIT -I{} bash -c \
+  "curl --silent {} | egrep -o -i 'david lee roth|sammy hagar' " \
+  > ${BAND_MEMBERS}
+}
+
+########################
+# MAIN EXECUTION POINT #
+########################
+parse_employee_urls
+get_band_members
 
 echo "The Right Answer Totals:"
 count_string_in_file "David"
 count_string_in_file "Sammy"
-

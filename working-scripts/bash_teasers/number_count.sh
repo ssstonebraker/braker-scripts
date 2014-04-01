8370e6a6f04ad35de2c4e178b7f2df5ddf6acccd
Steve Stonebraker <github@brakertech.com>
2014-04-01 00:06:54 -0500

    fixed counting

--- working-scripts/bash_teasers/number_count.sh
+++ working-scripts/bash_teasers/number_count.sh
@@ -7,10 +7,8 @@
 
 
 for x in {8000..9999} ; do
-	ocount $x
-
 	egrep "0{2,4}|1{2,4}|2{2,4}|3{2,4}|4{2,4}|5{2,4}|6{2,4}|7{2,4}|8{2,4}|9{2,4}" <<< "$x" &> /dev/null
-	[[ "$?" -eq 0 ]] && echo "number $1 has integer 0-9 occuring more than once"
+	[[ "$?" -eq 0 ]] && echo "number $x has integer 0-9 occuring more than once"
 
 done
 

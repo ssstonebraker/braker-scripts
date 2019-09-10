291ee3b840eec544038b1b7b8012bbdb59361ffe
Steve Stonebraker <github@brakertech.com>
2019-09-10 01:39:29 -0500

    initial commit

---
+++ working-scripts/aws-list-subets-all-profiles.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# Filename: aws-list-subets-all-profiles.sh
+# Description: print subnets from all vpcs across all profiles to a txt file
+# Usage: ./aws-list-subets-all-profiles.sh
+# Output: all_subnets.txt
+# Author: Steve Stonebraker
+
+aws_profiles=$( \
+        grep '\[profile' ~/.aws/config \
+        | awk '{sub(/]/, "", $2); print $2}' \
+)
+
+for profile in ${aws_profiles}
+do
+    echo "[*] - Processing profile [$profile]"
+    aws ec2 describe-subnets --profile ${profile} | jq -r '.Subnets[]|[.CidrBlock]|@tsv' | sort > subnets_${profile}.txt
+done
+
+echo "[*] - Processing default "
+aws ec2 describe-subnets | jq -r '.Subnets[]|[.CidrBlock]|@tsv' | sort > subnets_default.txt
+
+echo "[*] - combining all output"
+
+cat subnets*.txt | sort | uniq > all_subnets.txt
--- working-scripts/jenkins_delete_old_builds.sh
+++ working-scripts/jenkins_delete_old_builds.sh
@@ -7,4 +7,4 @@
 # Place in crontab
 # @daily /bin/bash -x /root/scripts/jenkins_cleanup.sh > /root/scripts/jenkins_cleanup.log
 
-find /var/lib/jenkins/jobs/*/builds/ -maxdepth 1 -mindepth 1 -mtime +30 -type d -regextype egrep -regex '^.*/[0-9]*$' -exec rm -rfv {} \;
+find /var/lib/jenkins/jobs/*/builds/ -maxdepth 1 -mindepth 1 -mtime +30 -type d -regextype egrep -regex '^.*/[0-9]*$' -exec rm -rfv {} \;   

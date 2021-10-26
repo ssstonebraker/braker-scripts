d07da3984f65991ae0bfe41a30810ee2c15d4ac9
Steve Stonebraker <github@brakertech.com>
2021-10-26 16:01:56 -0500

    Create fwtest_solariburst_domains.sh
    Performs a nslookup on all Solariburst malicious domains
    Purpose: To test Endpoint Detection Response and Network Monitoring Software

---
+++ working-scripts/fwtest_solariburst_domains.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Performs a nslookup on all Solariburst malicious domains
+# Purpose: To test Endpoint Detection Response and Network Monitoring Software
+nslookup avsvmcloud.com
+nslookup digitalcollege.org
+nslookup freescanonline.com
+nslookup deftsecurity.com
+nslookup thedoccloud.com
+nslookup websitetheme.com
+nslookup highdatabase.com
+nslookup incomeupdate.com
+nslookup databasegalore.com
+nslookup panhardware.com
+nslookup zupertech.com
+nslookup seobundlekit.com
+nslookup lcomputers.com
+nslookup virtualdataserver.com
+nslookup webcodez.com
+nslookup infinitysoftwares.com
+nslookup ervsystem.com
